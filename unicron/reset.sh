f90299f432ea70c5932f13674566d33eba0107c0
Michael Currin <michael.currin@gmail.com>
2020-01-04 11:08:14 +0200

    feat: Create reset.sh

---
+++ unicron/reset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Undo changes made by main file, for easy repeat testing.
+
+# No action should be needed for the fail.sh file since it can never succeed. But it is
+# included anyway in case the file is mistakingly created by the main script.
+
+cd var/last_run/
+
+echo $(date +%Y%m%d) >today.sh.txt
+echo "20200101" >old.sh.txt
+rm never_run_before.sh.txt >/dev/null 2>&1 || true
+rm fail.sh.txt >/dev/null 2>&1 || true
