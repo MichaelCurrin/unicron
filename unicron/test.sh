4196d2596514219e12797b7c3b4527249eb75788
Michael Currin <michael.currin@gmail.com>
2020-01-04 11:08:26 +0200

    feat: Create test.sh

---
+++ unicron/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Reset"
+echo "==="
+./reset.sh
+echo
+echo
+
+echo "Main - first run"
+echo "==="
+./unicron.sh
+echo
+echo
+
+echo "Main - second run"
+echo "==="
+./unicron.sh
