8c0f9a14a167735d20d3bbd367f7f2916c0bc03c
Michael Currin <michael.currin@gmail.com>
2020-01-12 21:33:49 +0200

    docs: Update comments

--- unicron/reset.sh
+++ unicron/reset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Undo changes made by main file, for easy repeat testing.
 #
-# No action should be needed for the fail.sh file since it can never succeed. But it is
-# included anyway in case the file is mistakingly created by the main script.
+# No action should be needed for the fail.sh file since it can never succeed.
+# But it is included anyway in case the file is mistakingly created by the main
+# script.
 
 cd _test_var/last_run/
 
