dbaea864ca50ce7ecb04311f5870e2c9033ef41f
Michael Currin <michael.currin@gmail.com>
2020-01-14 13:54:40 +0200

    refactor: Move test variable

--- unicron/test.sh
+++ unicron/test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
 # Run main app against testing directory using fixture data and show output.
 
-export TEST=1
-
 echo "Reset"
 echo "==="
 ./reset.sh
@@ -11,10 +9,10 @@ echo
 
 echo "Main - first run"
 echo "==="
-./unicron.py -v
+TEST=true ./unicron.py -v
 echo
 echo
 
 echo "Main - second run"
 echo "==="
-./unicron.py -v
+TEST=true ./unicron.py -v
