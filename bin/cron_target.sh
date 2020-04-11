c06cba4d8f31d19ff9b1ff74bedab19b916218ff
Michael Currin <michael.currin@gmail.com>
2020-04-11 21:59:09 +0200

    chore: Add script

---
+++ bin/cron_target.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Run Unicron as a cron command.
+#
+# On success, be silent.
+# On error, send all output to the user's local mailbox.
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+SCRIPT_FILEPATH="$SCRIPT_DIR/../unicron/unicron.py"
+
+OUTPUT="$($SCRIPT_FILEPATH 2>&1)"
+
+if [[ $? -ne 0 ]]; then
+    echo "$RESULT" | mail -s 'Unicron task failed!' $USER
+fi
