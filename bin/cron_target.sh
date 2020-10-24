ceee5bc6cdeaedc3311cb4e564fb4e68afc2ee3c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-24 20:30:45 +0200

    refactor: Update bin script

--- bin/cron_target.sh
+++ bin/cron_target.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-# Run Unicron as a cron command.
-#
-# On success, be silent.
-# On error, send all output to the user's local mailbox.
+# Run Unicron as a scheduled cron command.
 #
 # This script can be run from anywhere.
+#
+# On success, run silently - nothing printed at all.
+# On error, capture all stdout and stderr so it can be sent to user's local mailbox using cron's mechanism or the mail command.
+# Either way, you won't see anything printed if run this command.
 
 set -e
 
-SCRIPT_DIR=$(dirname $(realpath $0))
-SCRIPT_FILEPATH="$SCRIPT_DIR/../unicron/unicron.py"
+SCRIPT_DIR=$(dirname $(dirname $(realpath $0)))
+
+cd "$SCRIPT_DIR"
 
 set +e
-OUTPUT="$($SCRIPT_FILEPATH 2>&1)"
+
+CMD_OUTPUT="$(make run-quiet 2>&1)"
 
 if [[ $? -ne 0 ]]; then
-    set -e
-    echo "$OUTPUT" | mail -s 'Unicron task failed!' $USER
+  set -e
+  echo "$CMD_OUTPUT" | mail -s 'Unicron task failed!' $USER
 
-    exit 1
+  exit 1
 fi
 
 exit 0
