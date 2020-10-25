886ecbd748549d7f8ae5b7b1856c90a81bbf4d9e
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-25 15:47:20 +0200

    Update reset.sh

--- bin/reset.sh
+++ bin/reset.sh
@@ -10,14 +10,15 @@
 echo 'Entering _test_var directory.'
 cd unicron/_test_var
 
+echo 'Remove logs.'
+rm app.log >/dev/null 2>&1 || true
+rm output/*.log >/dev/null 2>&1 || true
+
 echo 'Create last-run file fixtures.'
 cd last_run/
+# Note that the today.sh task will never actually run when it unicron checks it,
+# so there today.sh.log will never get created and this is okay.
 echo $(date +%Y-%m-%d) >today.sh.txt
 echo "2020-01-01" >old.sh.txt
 rm never_run_before.sh.txt >/dev/null 2>&1 || true
 rm fail.sh.txt >/dev/null 2>&1 || true
-cd ..
-
-echo 'Remove logs.'
-rm app.log >/dev/null 2>&1 || true
-rm output/*.log >/dev/null 2>&1 || true
